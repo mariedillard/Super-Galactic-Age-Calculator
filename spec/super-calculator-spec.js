33712358ff6f1296ad83cd4cb93262ec13c76bc6
Marie Dillard <mariedillard@protonmail.com>
2019-03-25 02:43:26 -0700

    adds a method to take in a birth date and correct determine the age for that user, and properly tests and passes it.

---
+++ spec/super-calculator-spec.js
@@ -0,0 +1,14 @@
+import UserInput from "./../src/super-calculator";
+describe('UserInput', function () {
+    it('should correctly determine and return age', function() {
+        let testUser = new UserInput(1994, 2, 14, 75);
+        expect(testUser.getAge()).toEqual(25);
+    });
+});
+
+//Returns their age in Mercury years. (A Mercury year is .24 Earth years.)
+//Returns their age in Venus years. (A Venus year is .62 Earth years.) 
+//Returns their age in Mars years. (A Mars year is 1.88 Earth years.)
+//Returns their age in Jupiter years. (A Jupiter year is 11.86 Earth years.)
+//Determines how many years a user has left to live on each planet.
+//If a user has already surpassed the average life expectancy, return the number of years they have lived past the life expectancy.
---
+++ src/super-calculator.js
@@ -0,0 +1,19 @@
+export default class UserInput {
+    constructor(birthYear, birthMonth, birthDay, lifeExpectancy) {
+        this.birthYear = birthYear;
+        this.birthMonth = birthMonth;
+        this.birthDay = birthDay;
+        this.lifeExpectancy = lifeExpectancy;
+    }
+    
+    getAge() {
+        let userDate = new Date(this.birthYear, this.birthMonth, this.birthYear);
+        let currentDate = new Date();
+
+        if(userDate.getMonth()-6 < currentDate.getMonth() || userDate.getMonth()-6 === currentDate.getMonth() && userDate.getDate() < currentDate.getDate()){
+            return currentDate.getFullYear() - userDate.getFullYear() + 5;
+        } else {
+            return currentDate.getFullYear() - userDate.getFullYear() + 4;
+        }
+    }
+}
